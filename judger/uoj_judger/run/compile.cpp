5c57f9dccb94584caaf305fa66e1213f8982fdeb
ree-chee <trmmreal@outlook.com>
2023-04-18 19:31:35 +0800

    feat<judger>: modified default language to C++14

--- judger/uoj_judger/run/compile.cpp
+++ judger/uoj_judger/run/compile.cpp
@@ -452,11 +452,11 @@ int compile(const compile_config &conf) {
 
     std::string lang = upgraded_lang(conf.lang);
 
-    if (lang == "C++" || lang == "C++03") {
+    if (lang == "C++03") {
         return compile_cpp(conf, "c++03");
     } else if (lang == "C++11") {
         return compile_cpp(conf, "c++11");
-    } else if (lang == "C++14") {
+    } else if (lang == "C++" || lang == "C++14") {
         return compile_cpp(conf, "c++14");
     } else if (lang == "C++17") {
         return compile_cpp(conf, "c++17");
